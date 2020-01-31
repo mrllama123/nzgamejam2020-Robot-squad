38036f904499c35002893694948d174be33e5fff
Kodok-ki <37203704+Kodok-ki@users.noreply.github.com>
2020-02-01 11:28:28 +1300

    Adds script for bridges

---
+++ Robot Squad/Assets/Scripts/Bridge.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+
+    public GameObject[] bridges;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle()
+    {
+        if(bridges != null)
+        {
+            foreach(GameObject bridge in bridges)
+            {
+                bridge.SetActive(!bridge.activeInHierarchy);
+            }
+            
+       }
+    }   
+}
