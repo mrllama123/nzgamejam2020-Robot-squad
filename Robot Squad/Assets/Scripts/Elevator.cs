cba8a3231bdd3074dd28c501e2fa5a0ec67b4403
Tuwi Michael Nannings <mrnannings@gmail.com>
2020-02-01 08:42:58 +1300

    basic machinic;

---
+++ Robot Squad/Assets/Scripts/Elevator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator.enabled = false;
+    }
+}
