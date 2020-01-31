6d62fd83277658114313ab3b6ed4799182b164a8
Tuwi Michael Nannings <mrnannings@gmail.com>
2020-02-01 08:38:29 +1300

    basic mechanic;

---
+++ Robot Squad/Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type
+    {
+        Gear,
+        Gold,
+        Screw,
+        Copper,
+        Antenna,
+        Water,
+        Chip,
+        Computer
+    }
+
+    public Type type;
+    public Machine machine;
+}
