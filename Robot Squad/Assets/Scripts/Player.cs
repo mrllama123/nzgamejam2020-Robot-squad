4740fd9fbc93c526c127b5f2bb716f7075fcc463
Tuwi Michael Nannings <mrnannings@gmail.com>
2020-02-01 08:41:22 +1300

    Revert "basic mechanic;"
    This reverts commit 6d62fd83277658114313ab3b6ed4799182b164a8.

--- Robot Squad/Assets/Scripts/Player.cs
+++ Robot Squad/Assets/Scripts/Player.cs
@@ -12,10 +12,6 @@ public class Player : MonoBehaviour
     float checkGroundRadius;
     public LayerMask groundLayer;
     // Start is called before the first frame update
-
-    public Item itemInHands;
-    public Transform itemSlot;
-
     void Start()
     {
         checkGroundRadius = 0.5f;
@@ -26,61 +22,6 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Movement();
-
-        HandleItems();
-    }
-
-    private void HandleItems()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            //if you have a item check if there is a machine to put it in to if its fit
-            if (itemInHands != null)
-            {
-                Machine[] machines = FindObjectsOfType<Machine>();
-                foreach (Machine machine in machines)
-                {
-                    if (Vector2.Distance(machine.transform.position, transform.position) < 1)
-                    {
-                        if (machine.InsertItem(itemInHands))
-                        {
-                            itemInHands = null;
-                        }
-                        else
-                        {
-                            print("not the right item");
-                        }
-                    }
-                }
-            }
-            else
-            {
-                //if you don't have an item looks for all items in distance 
-                Item[] allItems = FindObjectsOfType<Item>();
-                foreach (Item item in allItems)
-                {
-                    if (Vector2.Distance(item.transform.position, transform.position) < 1)
-                    {
-                        if (itemInHands != item)
-                        {
-                            if (item.machine != null) //remove item from machin
-                            {
-                                item.machine.RemoveItem();
-                                item.machine = null;
-                            }
-                            itemInHands = item;
-                            item.transform.position = itemSlot.transform.position;
-                            item.transform.SetParent(itemSlot);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void Movement()
     {
         // collider 
         Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
@@ -98,7 +39,7 @@ public class Player : MonoBehaviour
         float moveBy = x * speed;
         rigidbody2D.velocity = new Vector2(moveBy, rigidbody2D.velocity.y);
         //jump logic
-        if (Input.GetAxisRaw("Vertical") > 0 && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
         }
