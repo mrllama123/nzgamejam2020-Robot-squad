aa2ce04b61a8edcd940f32423ce603b85eb3fca6
Tuwi Michael Nannings <mrnannings@gmail.com>
2020-02-01 16:18:09 +1300

    assets; scrolling background;

---
+++ Robot Squad/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float speed = 10;
+    private Material _material;
+    void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        _material.mainTextureOffset = new Vector2(Time.time * speed, 0);
+        //GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
