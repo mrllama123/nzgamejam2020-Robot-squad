01258d08516d68eef507bede7062fa8366198bf8
Kodok-ki <37203704+Kodok-ki@users.noreply.github.com>
2020-02-01 22:22:35 +1300

    Adds main menu and intro sequence

---
+++ Robot Squad/Assets/Scripts/OnClickStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OnClickStart : MonoBehaviour
+{
+    public GameObject TitleUI;
+
+
+    public void RemoveTitleScreen()
+    {
+        if (TitleUI != null)
+        {
+            foreach (Transform child in TitleUI.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Robot Squad/Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public float delay = 0.1f;
+    private string currentText = "";
+    private string fullText;
+
+    // Use this for initialization
+    void Start()
+    {
+        fullText = GetComponent<Text>().text;
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
